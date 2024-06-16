6724b853fcc4f47cdba6e78e3582e8ca4a618c63
aragami3070 <smirnov17612@gmail.com>
2024-06-17 00:00:45 +0400

    Delete two warns keymap

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -10,15 +10,7 @@ vim.keymap.set("n", "<space>d", ":Neotree right<CR>") -- toggle file explorer
 vim.keymap.set("n", "<space>r", ":Neotree close<CR>") -- toggle file explorer
 vim.keymap.set("n", "<space>gs", ":Neotree git_status<CR>") -- toggle file explorer
 
-vim.keymap.set("n", "]t", function()
-  require("todo-comments").jump_next()
-end, { desc = "Next todo comment" })
-
-vim.keymap.set("n", "[t", function()
-  require("todo-comments").jump_prev()
-end, { desc = "Previous todo comment" })
-
-
+-- Added open git tree
 vim.keymap.set("n", "<space>gt", ":Flog<CR>") -- git tree
 
 --Added new out from insert mode
--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -1,3 +1,3 @@
 require('diffview').setup({
-    
+
 })
