87ae8e82261e0f25dde9f2d6584a424eed6eef00
aragami3070 <smirnov17612@gmail.com>
2024-09-17 22:36:08 +0400

    Added html css typescript/js lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,6 +4,9 @@ require'lspconfig'.texlab.setup{}
 require'lspconfig'.neocmake.setup{}
 require'lspconfig'.markdown_oxide.setup{}
 require'lspconfig'.lua_ls.setup{}
+require'lspconfig'.ts_ls.setup{}
+require'lspconfig'.cssls.setup{}
+require'lspconfig'.html.setup{}
 require'lspconfig'.svelte.setup{}
 -- require'lspconfig'.kotlin_language_server.setup{}
 require'lspconfig'.pyright.setup{}
