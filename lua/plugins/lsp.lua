c6284af8884a3d93bb5e4f886cbd0f80edcd314f
aragami3070 <smirnov17612@gmail.com>
2024-09-26 22:30:34 +0400

    Relocate require('lspconfig')... in lsp.lua

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,14 +1,99 @@
-require'lspconfig'.clangd.setup{}
-require'lspconfig'.omnisharp.setup{}
-require'lspconfig'.texlab.setup{}
-require'lspconfig'.neocmake.setup{}
-require'lspconfig'.markdown_oxide.setup{}
-require'lspconfig'.lua_ls.setup{}
-require'lspconfig'.ts_ls.setup{}
-require'lspconfig'.cssls.setup{}
-require'lspconfig'.html.setup{}
-require'lspconfig'.asm_lsp.setup{}
-require'lspconfig'.typst_lsp.setup{}
-require'lspconfig'.svelte.setup{}
--- require'lspconfig'.kotlin_language_server.setup{}
-require'lspconfig'.pyright.setup{}
+  local navic = require("nvim-navic")
+  local capabilities = require('cmp_nvim_lsp').default_capabilities()
+  -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+  require('lspconfig')['clangd'].setup {
+    capabilities = capabilities,
+    on_attach = function(client, bufnr)
+        navic.attach(client, bufnr)
+    end
+  }
+
+  require('lspconfig')['omnisharp'].setup {
+    capabilities = capabilities,
+    on_attach = function(client, bufnr)
+        navic.attach(client, bufnr)
+    end
+  }
+
+  require('lspconfig')['texlab'].setup {
+    capabilities = capabilities,
+    on_attach = function(client, bufnr)
+        navic.attach(client, bufnr)
+    end
+  }
+
+  require('lspconfig')['neocmake'].setup {
+    capabilities = capabilities,
+    on_attach = function(client, bufnr)
+        navic.attach(client, bufnr)
+    end
+  }
+
+  require('lspconfig')['markdown_oxide'].setup {
+    capabilities = capabilities,
+    on_attach = function(client, bufnr)
+        navic.attach(client, bufnr)
+    end
+  }
+
+  require('lspconfig')['lua_ls'].setup {
+    capabilities = capabilities,
+    on_attach = function(client, bufnr)
+        navic.attach(client, bufnr)
+    end
+  }
+
+  require('lspconfig')['ts_ls'].setup {
+    capabilities = capabilities,
+    on_attach = function(client, bufnr)
+        navic.attach(client, bufnr)
+    end
+  }
+
+  require('lspconfig')['cssls'].setup {
+    capabilities = capabilities,
+    on_attach = function(client, bufnr)
+        navic.attach(client, bufnr)
+    end
+  }
+
+  require('lspconfig')['html'].setup {
+    capabilities = capabilities,
+    on_attach = function(client, bufnr)
+        navic.attach(client, bufnr)
+    end
+  }
+
+  require('lspconfig')['asm_lsp'].setup {
+    capabilities = capabilities,
+    on_attach = function(client, bufnr)
+        navic.attach(client, bufnr)
+    end
+  }
+
+  -- require('lspconfig')['typst_lsp'].setup {
+  --   capabilities = capabilities,
+  --   on_attach = function(client, bufnr)
+  --       navic.attach(client, bufnr)
+  --   end
+  -- }
+
+  require('lspconfig')['svelte'].setup {
+    capabilities = capabilities,
+    on_attach = function(client, bufnr)
+        navic.attach(client, bufnr)
+    end
+
+  }
+
+  -- require('lspconfig')['kotlin_language_server'].setup {
+  --   capabilities = capabilities
+  -- }
+
+  require('lspconfig')['pyright'].setup {
+    capabilities = capabilities,
+    on_attach = function(client, bufnr)
+        navic.attach(client, bufnr)
+    end
+  }
+
