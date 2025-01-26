cf0358be1c8181df553d5ddbdd47770e4b1836b4
aragami3070 <smirnov17612@gmail.com>
2025-01-26 14:28:19 +0400

    Delete: document symbols
    Neotree remove document symbols from their plugin :(, but I have Telescope lsp_dynamic workspace_symbols :) keymap = <space>ls in n mode :)

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -8,7 +8,7 @@ vim.fn.sign_define("DiagnosticSignHint",
 {text = "󰌵", texthl = "DiagnosticSignHint"})
 
 require("neo-tree").setup({
-    sources = { "filesystem", "git_status", "document_symbols" },
+    sources = { "filesystem", "git_status"},
     position = "right",
     cmd = { "Neotree" },
     filesystem = {
@@ -18,41 +18,4 @@ require("neo-tree").setup({
             hide_gitignored = true,
         },
     },
-    document_symbols = {
-          follow_cursor = true,
-          client_filters = "all",
-          window = {
-            mappings = {
-              ["<CR>"] = "toggle_node",
-            },
-          },
-          -- kinds = {
-          --   File = { icon = "", hl = "@text" },
-          --   Module = { icon = "", hl = "@text" },
-          --   Namespace = { icon = "", hl = "@namespace" },
-          --   Package = { icon = "", hl = "@string" },
-          --   Class = { icon = "", hl = "@type" },
-          --   Method = { icon = "", hl = "@method" },
-          --   Property = { icon = "", hl = "@property" },
-          --   Field = { icon = "", hl = "@field" },
-          --   Constructor = { icon = " ", hl = "@constructor" },
-          --   Enum = { icon = "", hl = "@type" },
-          --   Interface = { icon = "", hl = "@type" },
-          --   Function = { icon = "", hl = "@function" },
-          --   Variable = { icon = "", hl = "@variable" },
-          --   Constant = { icon = "", hl = "@constant" },
-          --   String = { icon = "", hl = "@string" },
-          --   Number = { icon = "", hl = "@number" },
-          --   Boolean = { icon = "", hl = "@boolean" },
-          --   Array = { icon = "", hl = "@variable" },
-          --   Object = { icon = "", hl = "@type" },
-          --   Key = { icon = "", hl = "@variable" },
-          --   -- Null = { icon = "ﳠ", hl = "@boolean" },
-          --   EnumMember = { icon = "", hl = "@property" },
-          --   Struct = { icon = "", hl = "@type" },
-          --   Event = { icon = "", hl = "@variable.builtin" },
-          --   Operator = { icon = "", hl = "@operator" },
-          --   TypeParameter = { icon = "", hl = "@type" },
-          -- },
-        },
 })
