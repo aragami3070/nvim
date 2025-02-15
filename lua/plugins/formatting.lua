72f702ba88b6ed9b007e44e7ac40a5de61497418
aragami3070 <smirnov17612@gmail.com>
2025-02-16 03:23:43 +0400

    Fix: formatting

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -1,10 +1,10 @@
-require("conform").setup{
+require("conform").setup {
     formatters_by_ft = {
         csharp = { "csharpier" },
-      },
-      format_on_save = {
-        lsp_fallback = true,
-        async = false,
-        timeout_ms = 500,
-      },
+    },
+    -- format_on_save = {
+    --   lsp_fallback = true,
+    --   async = false,
+    --   timeout_ms = 500,
+    -- },
 }
