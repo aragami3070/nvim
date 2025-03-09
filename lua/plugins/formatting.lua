584092de542c8884bd2e068da4b12900722699a4
aragami3070 <smirnov17612@gmail.com>
2025-03-09 13:30:15 +0400

    Add: formatter for js, ts, vue, html, css

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -1,18 +1,23 @@
 require("conform").setup({
-    formatters_by_ft = {
-        csharp = { "csharpier" },
-        lua = { "stylua" },
-        cpp = { "clang_format" },
-        typ = { "typstfmt" },
-    },
-    formatters = {
-        clang_format = {
-            prepend_args = { "-style=file:" .. vim.fn.expand("~/.config/nvim/.clang-format") },
-        },
-    },
-    -- format_on_save = {
-    --   lsp_fallback = true,
-    --   async = false,
-    --   timeout_ms = 500,
-    -- },
+	formatters_by_ft = {
+		csharp = { "csharpier" },
+		lua = { "stylua" },
+		cpp = { "clang_format" },
+		typ = { "typstfmt" },
+		vue = { "prettier" },
+		js = { "prettier" },
+		ts = { "prettier" },
+		html = { "prettier" },
+		css = { "prettier" },
+	},
+	formatters = {
+		clang_format = {
+			prepend_args = { "-style=file:" .. vim.fn.expand("~/.config/nvim/.clang-format") },
+		},
+	},
+	-- format_on_save = {
+	--   lsp_fallback = true,
+	--   async = false,
+	--   timeout_ms = 500,
+	-- },
 })
