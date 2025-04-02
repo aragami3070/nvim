9bf3d6d46bc8e7113102e1ce3f30890cd6d1811b
aragami3070 <smirnov17612@gmail.com>
2025-04-02 17:52:22 +0400

    Fix: telescope, remove version

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -63,7 +63,7 @@ require("lazy").setup({
 		-- Fuzzy search
 		{
 			"nvim-telescope/telescope.nvim",
-			version = "0.1.5",
+			-- version = "0.1.5",
 			-- or                            , branch = '0.1.x',
 			dependencies = { { "nvim-lua/plenary.nvim" } },
 		},
