1d23b2b75f0ce171539c212bb054f1f467ece3fc
aragami3070 <smirnov17612@gmail.com>
2025-04-12 13:13:44 +0400

    Add: lua_ls for nvim config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -40,6 +40,23 @@ require("lspconfig")["markdown_oxide"].setup({
 })
 
 require("lspconfig")["lua_ls"].setup({
+	settings = {
+		Lua = {
+			runtime = {
+				version = "LuaJIT",
+			},
+			diagnostics = {
+				globals = { "vim" },
+			},
+			workspace = {
+				library = vim.api.nvim_get_runtime_file("", true),
+				checkThirdParty = false,
+			},
+			telemetry = {
+				enable = false,
+			},
+		},
+	},
 	capabilities = capabilities,
 	on_attach = function(client, bufnr)
 		navic.attach(client, bufnr)
