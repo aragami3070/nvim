1da9a41dbf9f72b9c9d3ec9f18faa3b87f247120
aragami3070 <smirnov17612@gmail.com>
2025-05-20 23:16:39 +0400

    Add: flash.nvim

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -515,6 +515,20 @@ require("lazy").setup({
 				end
 			end,
 		},
+		{
+			"folke/flash.nvim",
+			event = "VeryLazy",
+			---@type Flash.Config
+			opts = {},
+			-- stylua: ignore
+			 keys = {
+				{ "<leader>j", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+				{ "<leader>k", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+				{ "<leader>J", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+				{ "<leader>K", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+				{ "<leader>,", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+			},
+		},
 
 		{
 			"rcarriga/nvim-dap-ui",
