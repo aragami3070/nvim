546784ebcb09d48ea0cd104624330ae10d1f53db
aragami3070 <smirnov17612@gmail.com>
2025-05-21 21:16:06 +0400

    Chore: delete omnisharp lsp config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,5 +1,3 @@
-local pid = vim.fn.getpid()
-
 local navic = require("nvim-navic")
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
@@ -9,14 +7,6 @@ require("lspconfig")["clangd"].setup({
 		navic.attach(client, bufnr)
 	end,
 })
-
--- require("lspconfig")["omnisharp"].setup({
--- 	capabilities = capabilities,
--- 	cmd = { "/home/aragami3070/.local/share/nvim/mason/bin/omnisharp", "--languageserver", "--hostPID", tostring(pid) },
--- 	on_attach = function(client, bufnr)
--- 		navic.attach(client, bufnr)
--- 	end,
--- })
 
 require("lspconfig")["texlab"].setup({
 	capabilities = capabilities,
