51bf8a60a8ce36f90075aa5fb76b40ab71626e71
aragami3070 <smirnov17612@gmail.com>
2025-07-06 00:43:30 +0400

    Feat: add run rust debug with args

--- lua/plugins/nvim-dap-rust.lua
+++ lua/plugins/nvim-dap-rust.lua
@@ -1,37 +1,61 @@
 local dap = require("dap")
-    if not dap.adapters["codelldb"] then
-      require("dap").adapters["codelldb"] = {
-        type = "server",
-        host = "127.0.0.1",
-        port = "${port}",
-        executable = {
-          command = "codelldb",
-          args = {
-            "--port",
-            "${port}",
-          },
-        },
-      }
-    end
-    for _, lang in ipairs({ "rust" }) do
-      dap.configurations[lang] = {
-        {
-          type = "codelldb",
-          request = "launch",
-          name = "Launch file",
-          program = function()
-            return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/target/debug/".. vim.fn.substitute(vim.fn.getcwd(), '^.*/', '', ''), "file")
-          end,
-          cwd = "${workspaceFolder}",
-        },
-        {
-          type = "codelldb",
-          request = "attach",
-          name = "Attach to process",
-          pid = function()
-            return vim.fn.input("Input pid: ")
-          end,
-          cwd = "${workspaceFolder}",
-        },
-      }
-    end
+if not dap.adapters["codelldb"] then
+	require("dap").adapters["codelldb"] = {
+		type = "server",
+		host = "127.0.0.1",
+		port = "${port}",
+		executable = {
+			command = "codelldb",
+			args = {
+				"--port",
+				"${port}",
+			},
+		},
+	}
+end
+for _, lang in ipairs({ "rust" }) do
+	dap.configurations[lang] = {
+		{
+			type = "codelldb",
+			request = "launch",
+			name = "Launch file",
+			program = function()
+				return vim.fn.input(
+					"Path to executable: ",
+					vim.fn.getcwd() .. "/target/debug/" .. vim.fn.substitute(vim.fn.getcwd(), "^.*/", "", ""),
+					"file"
+				)
+			end,
+			cwd = "${workspaceFolder}",
+		},
+		{
+			type = "codelldb",
+			request = "launch",
+			name = "Launch with args",
+			program = function()
+				return vim.fn.input(
+					"Path to executable: ",
+					vim.fn.getcwd() .. "/target/debug/" .. vim.fn.substitute(vim.fn.getcwd(), "^.*/", "", ""),
+					"file"
+				)
+			end,
+			args = function()
+				local args = {}
+				vim.ui.input({ prompt = "args: " }, function(input)
+					args = vim.split(input, " ")
+				end)
+				return args
+			end,
+			cwd = "${workspaceFolder}",
+		},
+		{
+			type = "codelldb",
+			request = "attach",
+			name = "Attach to process",
+			pid = function()
+				return vim.fn.input("Input pid: ")
+			end,
+			cwd = "${workspaceFolder}",
+		},
+	}
+end
