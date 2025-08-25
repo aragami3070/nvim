9eafd0eb7b023750b8186613a12c8618ff71e3e8
aragami3070 <smirnov17612@gmail.com>
2025-08-25 21:21:57 +0400

    Chore(lazy.nvim): add ui border rounded

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -619,4 +619,7 @@ require("lazy").setup({
 	-- install = { colorscheme = { "habamax" } },
 	-- automatically check for plugin updates
 	checker = { enabled = true },
+	ui = {
+		border = "rounded"
+	}
 })
