cc8e717a895496309e9fcbeb1fc8cdf743ef9d92
aragami3070 <smirnov17612@gmail.com>
2025-12-21 17:52:44 +0400

    Feat(telescope): add better find_command for find_files

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,1 +1,9 @@
-require('telescope').setup{}
+require("telescope").setup({
+    pickers = {
+        find_files = {
+            find_command = { "rg", "--files", "--color", "never", "-g", "!.git" },
+            hidden = true,
+            no_ignore = false,
+        },
+    },
+})
