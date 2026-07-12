84ece0b36f7b7e817aefe50b3ddda80fabce9d37
aragami3070 <smirnov17612@gmail.com>
2026-07-12 20:35:54 +0400

    feat(treesitter): add auto install parsers

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -23,9 +23,21 @@ require("nvim-treesitter").install(parsers_to_install)
 vim.api.nvim_create_autocmd("FileType", {
     pattern = "*",
     callback = function(args)
+        local ft = vim.bo[args.buf].filetype
         -- Ignore latex
-        if vim.bo[args.buf].filetype == "tex" or vim.bo[args.buf].filetype == "latex" then
+        if ft == "tex" or ft == "latex" then
             return
+        end
+        local lang = vim.treesitter.language.get_lang(ft) or ft
+        local parsers = require("nvim-treesitter.parsers")
+        if parsers[lang] then
+            local installed = require("nvim-treesitter.config").get_installed()
+            if not vim.list_contains(installed, lang) then
+                vim.notify("Installing treesitter parser for: " .. lang)
+                pcall(function()
+                    require("nvim-treesitter").install({ lang })
+                end)
+            end
         end
         pcall(vim.treesitter.start, args.buf)
     end,
