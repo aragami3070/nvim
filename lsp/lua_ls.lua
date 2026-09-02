606288c24b9d2a5aa0937c0cbe859998ed8daeb0
aragami3070 <smirnov17612@gmail.com>
2026-09-02 21:25:18 +0400

    feat(lsp): add hyprland support

--- lsp/lua_ls.lua
+++ lsp/lua_ls.lua
@@ -1,22 +1,25 @@
 return {
-	cmd = { "lua-language-server" },
-	filetypes = { "lua" },
-	root_markers = { ".luarc.json", ".luarc.jsonc" },
-	settings = {
-		Lua = {
-			runtime = {
-				version = "LuaJIT",
-			},
-			diagnostics = {
-				globals = { "vim" },
-			},
-			workspace = {
-				library = vim.api.nvim_get_runtime_file("", true),
-				checkThirdParty = false,
-			},
-			telemetry = {
-				enable = false,
-			},
-		},
-	},
+    cmd = { "lua-language-server" },
+    filetypes = { "lua" },
+    root_markers = { ".luarc.json", ".luarc.jsonc", ".git" },
+    settings = {
+        Lua = {
+            runtime = {
+                version = "LuaJIT",
+            },
+            diagnostics = {
+                globals = { "vim", "hl" },
+            },
+            workspace = {
+                library = vim.list_extend(vim.api.nvim_get_runtime_file("", true), { "/usr/share/hypr/stubs" }),
+                checkThirdParty = false,
+            },
+            telemetry = {
+                enable = false,
+            },
+            completion = {
+                callSnippet = "Replace",
+            },
+        },
+    },
 }
